e9d052fe0163b7122224d4d2f91cbd4982048ca9
xXangelXx <angeluchimag17@gmail.com>
2020-12-08 20:21:12 -0400

    delete comments

--- src/app/modules/weather/weater-component/weater-component.component.ts
+++ src/app/modules/weather/weater-component/weater-component.component.ts
@@ -8,6 +8,8 @@ import axios from 'axios';
 })
 export class WeaterComponentComponent implements OnInit {
   data:any 
+  city:any
+  //init
   async getFunction(){
      let data = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=8a6c09d5b598e9d813b3a4396460167b').then(
       (response)=>{
@@ -25,12 +27,18 @@ export class WeaterComponentComponent implements OnInit {
      await this.getFunction().then((res)=>{ this.data = res })
      console.log(this.data)
   };
+//algo asi
+  async getShow(){
+    let data = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=8a6c09d5b598e9d813b3a4396460167b').then(
+      (response)=>{
+        return response.data
+      }
+    ).catch((error)=>{
+      console.error(error)
+    })
+    this.city = data
+  }
   
-  /*ngOnChanges(changes: SimpleChange) {
-    // changes.prop contains the old and the new value...
-    console.log(changes)
-  }*/
-
 
 }/*
 try{
